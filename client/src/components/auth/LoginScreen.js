f17a1bd1680c3b83089cc3c4e6ae512df72e4766
jose arcani <juanjose.arcani2309@gmail.com>
2022-01-05 23:06:16 -0400

    styles folders and html login without styles

--- client/src/components/auth/LoginScreen.js
+++ client/src/components/auth/LoginScreen.js
@@ -3,7 +3,31 @@ import React from 'react';
 export const LoginScreen = () => {
   return (
     <div>
-      Login
+      <form>
+        <label>Login
+          <input
+            type="email"
+            placeholder="email"
+            autoComplete="off"
+            name="email"
+          />
+          <i class="fa fa-user"></i>
+        </label>
+        <label>Password
+          <input
+            type="password"
+            placeholder="password"
+            autoComplete="off"
+            name="password"
+          />
+          <i class="fa fa-key"></i>
+        </label>
+
+        <a href="/">Forgot your password?</a>
+        <button className="btn">
+          Login
+        </button>
+      </form>
     </div>
   )
 }
