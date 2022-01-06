cefc0583941c58bc85a720ad5d1d7ba70d1739e9
jose arcani <juanjose.arcani2309@gmail.com>
2022-01-06 00:12:07 -0400

    login screen completed

--- client/src/components/auth/LoginScreen.js
+++ client/src/components/auth/LoginScreen.js
@@ -1,33 +1,46 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+// https://codepen.io/boudra/pen/YXzLBN
+// https://www.lullabot.com/articles/bem-atomic-design-a-css-architecture-worth-loving
+//https://github.com/john-smilga/javascript-basic-projects/blob/master/04-navbar/final/styles.css
+//https://codepen.io/TurkAysenur/pen/ZEbXoRZ?editors=1000
 
 export const LoginScreen = () => {
   return (
-    <div>
+    <>
+      <h3 className="auth__title">Chat App</h3>
+      <div className="underline"></div>
       <form>
-        <label>Login
+        <div className="auth__input-container">
           <input
             type="email"
             placeholder="email"
             autoComplete="off"
+            className="auth__input"
             name="email"
           />
-          <i class="fa fa-user"></i>
-        </label>
-        <label>Password
+          <i className="fa fa-user"></i>
+        </div>
+        <div className="auth__input-container">
           <input
             type="password"
             placeholder="password"
             autoComplete="off"
+            className="auth__input"
             name="password"
-          />
-          <i class="fa fa-key"></i>
-        </label>
+            />
+          <i className="fa fa-key"></i>
+        </div>
 
-        <a href="/">Forgot your password?</a>
-        <button className="btn">
+        <a href="/" className="link mb-1">Forgot your password?</a>
+        <button className="btn btn-primary btn-block" type="submit">
           Login
         </button>
+        <Link to="/auth/register" className="link">
+          Create new account
+        </Link>
       </form>
-    </div>
+    </>
   )
 }
