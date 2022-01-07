77f88a5a532c49784542b664c6db119f9f84c06a
jose arcani <juanjose.arcani2309@gmail.com>
2022-01-06 22:34:30 -0400

    frontend completed

--- client/src/components/chat/ChatDetails.js
+++ client/src/components/chat/ChatDetails.js
@@ -2,8 +2,14 @@ import React from 'react';
 
 export const ChatDetails = () => {
   return (
-    <div className="chat__details">
-      !!!!!!chat details of chatt
+    <div className="chat__details-container">
+      <div className="chat-details__header">
+        <figure>
+          <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/3364143/download+%283%29+%281%29.png" alt="" />
+        </figure>
+        <h2>College Group</h2>
+        <p>Created by @user1, 1 jan 2022</p>
+      </div>
     </div>
   )
 }
