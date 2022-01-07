32b06c798dbc6c6bcd428369e718faab17676581
jose arcani <juanjose.arcani2309@gmail.com>
2022-01-07 10:19:39 -0400

    useForm hook and form validation for RegisterScreen

--- client/src/components/auth/RegisterScreen.js
+++ client/src/components/auth/RegisterScreen.js
@@ -1,12 +1,47 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
+import { useForm } from '../../hooks/useForm';
 
 export const RegisterScreen = () => {
+
+  const { data: user, handleChange, handleSubmit, errors } = useForm({
+    validations: {
+      name: {
+        pattern: {
+          value: '^[A-Za-z]*$',
+          message: 'Do not use special characters',
+        },
+      },
+      email: {
+        pattern: {
+          value: '^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$',
+          message: 'Check your email again'
+        }
+      },
+      password: {
+        required: {
+          value: true,
+          message: 'This is required'
+        },
+        custom: {
+          isValid: (value) => value.length > 5,
+          message: 'Needs to be at least 6 characters'
+        },
+      },
+    },
+    initialValues: {
+      name: 'josh',
+      email: 'test1@test.com',
+      password: 'secret',
+    },
+    onSubmit: () =>  <Redirect to="/auth/login" />
+  });
+
   return (
     <>
       <h3 className="auth__title">Chat App</h3>
       <div className="underline"></div>
-      <form>
+      <form onSubmit={ handleSubmit }>
         <div className="auth__input-container">
           <input
             type="text"
@@ -14,8 +49,11 @@ export const RegisterScreen = () => {
             autoComplete="off"
             className="auth__input input"
             name="name"
-          />
+            value={ user.name || '' }
+            onChange={ handleChange('name') }
+            />
           <i className="fa fa-user"></i>
+          { errors.name && <span className="auth__input--error">{errors.name}</span> }
         </div>
         <div className="auth__input-container">
           <input
@@ -24,8 +62,11 @@ export const RegisterScreen = () => {
             autoComplete="off"
             className="auth__input input"
             name="email"
+            value={ user.email || '' }
+            onChange={ handleChange('email') }
           />
           <i className="fa fa-envelope"></i>
+          { errors.email && <span className="auth__input--error">{errors.email}</span> }
         </div>
         <div className="auth__input-container">
           <input
@@ -34,24 +75,17 @@ export const RegisterScreen = () => {
             autoComplete="off"
             className="auth__input input"
             name="password"
+            value={ user.password || '' }
+            onChange={ handleChange('password') }
             />
           <i className="fa fa-key"></i>
-        </div>
-        <div className="auth__input-container">
-          <input
-            type="password"
-            placeholder="repeat password"
-            autoComplete="off"
-            className="auth__input input"
-            name="password2"
-            />
-          <i className="fa fa-key"></i>
+          { errors.password && <span className="auth__input--error">{errors.password}</span> }
         </div>
         <button className="btn btn-primary btn-block" type="submit">
           Register
         </button>
         <Link to="/auth/login" className="link">
-          Already have an acoount?
+          Already have an account?
         </Link>
       </form>
     </>
