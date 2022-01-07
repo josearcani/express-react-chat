7609a3145c9a1610093030fd2d56fa38e1dd493d
jose arcani <juanjose.arcani2309@gmail.com>
2022-01-06 22:56:48 -0400

    create context for auth

--- client/src/ChatApp.js
+++ client/src/ChatApp.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import { AppRouter } from './routers/AppRouter';
 
+import { authReducer } from './auth/authReducer';
+import { AuthContext } from './auth/authContext';
+
+
+const init = () => ({ logged: false });
+
 export const ChatApp = () => {
+
+  const [user, dispatch] = useReducer(authReducer, {}, init)
+
   return (
-    <>
+    <AuthContext.Provider value={{
+      user,
+      dispatch
+    }}>
       <AppRouter />
-    </>
+    </AuthContext.Provider>
   )
 }
