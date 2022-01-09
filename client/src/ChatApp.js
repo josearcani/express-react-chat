b92ef96a4b3daecda15b70bf0128c28405ca6006
jose arcani <juanjose.arcani2309@gmail.com>
2022-01-09 00:14:57 -0400

    Login form validation, accion auth and save session to localStorage

--- client/src/ChatApp.js
+++ client/src/ChatApp.js
@@ -1,15 +1,22 @@
-import React, { useReducer } from 'react';
+import React, { useEffect, useReducer } from 'react';
 import { AppRouter } from './routers/AppRouter';
 
 import { authReducer } from './auth/authReducer';
 import { AuthContext } from './auth/authContext';
 
 
-const init = () => ({ logged: false });
+const init = () => {
+  return JSON.parse(localStorage.getItem('user')) || { logged: false };
+};
 
 export const ChatApp = () => {
 
   const [user, dispatch] = useReducer(authReducer, {}, init)
+
+  useEffect(() => {
+    if (!user) return
+    localStorage.setItem('user', JSON.stringify(user))
+  }, [ user ])
 
   return (
     <AuthContext.Provider value={{
---
+++ client/src/actions/auth.js
@@ -0,0 +1,11 @@
+import { types } from "../types/types";
+
+export const login = (user) => ({
+  type: types.login,
+  payload: {
+    name: user.name,
+    email: user.email,
+    img: user.img,
+    uid: user.uid
+  }
+})
